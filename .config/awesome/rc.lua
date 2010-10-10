5b795f459b5278a72db1d3f23a9aa3d3fdf77ea0
Noah K. Tilton <binaural@gmail.com>
2010-10-09 23:12:28 -0500

    don't break firefox alt+{Left,Right} key bindings

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -209,8 +209,8 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
+    awful.key({ modkey, "Control" }, "Left",   awful.tag.viewprev       ),
+    awful.key({ modkey, "Control" }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
     awful.key({ modkey,           }, "j", function ()
             awful.client.focus.byidx( 1)
