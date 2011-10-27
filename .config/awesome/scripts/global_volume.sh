e044b7978e3a4bbfa46f0b382e214480e6265082
noah <noahktilton@gmail.com>
2011-10-27 04:16:49 -0500

    date hacks

--- .config/awesome/scripts/global_volume.sh
+++ .config/awesome/scripts/global_volume.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-# adjust either desktop PCM or stereo system volume (via squeezebox
-# server) depending upon which one is currently playing =)  this is
-# intended to be an awesome keyboard shortcut callback
+# adjust desktop PCM and squeezebox system volume
+# this is intended to be an awesome keyboard shortcut callback
 #
 # yes, I am lazy
+# scriptable consumer electronics are sooooooooooooooooooo rad
+# http://7be:9000/html/docs/cli-api.html?player=#mixer%20volume
 
 if [[ $# -ne 1 ]]; then
-  exit 1
+  exit -1
 fi
 
-#cmus-remote -Q
-#if [[ $? -eq 0 ]]; then
 amixer -Dpulse -q -c 0 set Master 1$1
-#else
-# http://downbe:9000/html/docs/cli-api.html?player=#mixer%20volume
 HOST=7be
 PORT=9090
 VOL_STEP=5
-
-# scriptable consumer electronics are sooooooooooooooooooo rad
-
-#player_id=$((echo "player id ?"; sleep 1)|telnet $HOST $PORT|sed 's/player id %3F //')
-#echo "$player_id"
 player_id="00:04:20:12:97:e5"
-(echo "$player_id mixer volume $1${VOL_STEP}"; sleep 1)|telnet $HOST $PORT
-#fi
+
+# Don't send commands to the squeezebox in the middle of the night,
+# people might be sleeping
+#
+HOUR=$(date +"%H")
+if [[ $HOUR -gt 07 && $HOUR -lt 02 ]]; then
+  (echo "$player_id mixer volume $1${VOL_STEP}"; sleep 1)|telnet $HOST $PORT
+fi
