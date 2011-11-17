1f94bf090dbf2841da66ac41ffd3563e970a60a1
noah <noahktilton@gmail.com>
2011-11-16 19:28:02 -0600

    themez

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -21,7 +21,8 @@ script_dir = table.concat({config_dir, "scripts"}, "/")
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
 -- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.init(config_dir .. "/themes/esq/theme.lua")
+beautiful.init(config_dir .. "/themes/theme.lua")
+--                                    ^ N.B. symlink
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvtc"
